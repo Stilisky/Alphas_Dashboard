f4a535fa27bd3d644cbd38df8bd76f0c3d670852
stilisky <setonde.hounnouvi@epitech.eu>
2023-08-30 08:42:04 +0100

    Added vue files

---
+++ client/src/components/userdashboard.js
@@ -0,0 +1,12 @@
+import UserNavbar from "./userNavbar";
+import SidebarUser from "./userSideBar";
+// import Sidebar from "./userSideBar";
+
+export function UserDashboard() {
+   return (
+      <div>
+         {/* <UserNavbar/> */}
+         <SidebarUser/>
+      </div>
+   )
+}
