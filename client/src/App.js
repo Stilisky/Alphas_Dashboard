bb1e0737dadd08bec56e4ff4caf8db92ddfe29f2
stilisky <setonde.hounnouvi@epitech.eu>
2023-08-31 11:14:26 +0100

    Conflict bugs fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import Landing from './components/landing';
 import AdminHome from './admin/page/AdminHome';
 import { Route, Routes } from 'react-router-dom';
-
-
+import { UserDashboard } from './components/userdashboard';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />}/>
       <Route path='/AdminHome' element={<AdminHome />}/>
-
+      <Route path='/dashboard' element={<UserDashboard />}/>
     </Routes>
-
-    
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './/styles/index.css';
-import App from './/components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
