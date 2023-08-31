d39f80571df2e1c19f47c9c41eb87809dc5a68c1
Lawane DJIBRILA <lawane.djibrila@epitech.eu>
2023-08-31 13:13:41 +0100

    fusion interface

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Landing from './components/landing';
 import AdminHome from './admin/page/AdminHome';
 import { Route, Routes } from 'react-router-dom';
 import { UserDashboard } from './components/userdashboard';
+import ServInterface from './components/serviceinterface';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path='/' element={<Landing />}/>
       <Route path='/AdminHome' element={<AdminHome />}/>
       <Route path='/dashboard' element={<UserDashboard />}/>
+      <Route path='/setting' element={<ServInterface />}/>
     </Routes>
   );
 }
